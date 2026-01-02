e453a720c163911983438efea5b03e46f4609da1
sajaysharma <ajasharma9761@gmail.com>
2026-01-02 22:48:57 +0530

    React Practice project Added

--- React_Js/Project_01/src/App.jsx
+++ React_Js/Project_01/src/App.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
 
 
 function App() {
+
+  let [counter, setCounter] = useState(10);
+
+  
+  const decNumber = () =>{
+    counter -= 1;
+    if(counter >= 0){
+      setCounter(counter);
+    }else{
+      console.log("Con't access")
+    }
+  }
+
+  const addNumber = () => {
+    counter = counter + 1;
+    if(counter <= 20){
+      setCounter(counter);
+    }else{
+      console.log("con't add more than 20");
+    }
+  }
   return(
-    <h1>Hello Guy's</h1>
+    <>
+       <h1>Counter Project "Why we use Hooks!" </h1>
+       <h1>Counter Value : {counter}</h1>
+       <button onClick={addNumber} >Click me</button>
+       <br /><br />
+       <button onClick={decNumber}>Remove me</button>
+    </>
   )
 }
 
